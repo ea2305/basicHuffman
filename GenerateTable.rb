6328d0ea0bb821a119904de1feee65c9c1473d0e
Elihu Cruz <ea_2305@hotmail.com>
2016-02-11 19:34:15 -0600

    clonado de parametro recivido

--- GenerateTable.rb
+++ GenerateTable.rb
@@ -10,13 +10,18 @@ class GenerateTable
         @masterArray = array
     end
 
-    def get_Table(values)
-        s_data = values
-        c_data = s_data.split("")
-        puts("#{c_data}")
+    def get_Table(value, elements)
+        arr_data = values.clone #recivimos el arreglo de elementos y copiamos
+
+        for x in elements
+
+        end
+
     end
 end
 
 gen = GenerateTable.new()#generamos la instancia del objeto
 
-puts("#{gen.loadTable("Elihu")}")
+elements = ["a","d","c"]
+
+gen.get_Table(elements)
