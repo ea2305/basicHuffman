c21dac80d38c404363985e1bfa8694673b795a1a
Elihu Cruz <ea_2305@hotmail.com>
2016-02-11 19:21:19 -0600

    Cambios, he instancia de clase

--- GenerateTable.rb
+++ GenerateTable.rb
@@ -10,11 +10,13 @@ class GenerateTable
         @masterArray = array
     end
 
-
-    def get_Table()
-        s_data = ""
+    def get_Table(values)
+        s_data = values
         c_data = s_data.split("")
-
         puts("#{c_data}")
     end
 end
+
+gen = GenerateTable.new()#generamos la instancia del objeto
+
+puts("#{gen.loadTable("Elihu")}")
